b10aba53f351ce900cd2c7b90a822695d31d58d0
Mostafa rayan <mr3309296@gmail.com>
2024-10-05 11:54:43 +0300

    Initial commit from Create Next App

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkBg: "#0e1217",
+        cardDark: "#1c1f26",
+        textSmDark: "#a8b3cf",
+      },
+      container: {
+        center: "true",
+        padding: "2rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
